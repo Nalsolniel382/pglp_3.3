71ce5f56890e681d49d36fb37933688c51c26c03
Utilisateur <user@debian.arcahe.ovh>
2020-03-11 16:55:39 +0100

    les classe robot robotstatique et direction

--- exo3_3/src/main/java/fr/uvsq/exo3_3/Robot.java
+++ exo3_3/src/main/java/fr/uvsq/exo3_3/Robot.java
@@ -1,5 +1,20 @@
 package fr.uvsq.exo3_3;
 
 public class Robot {
+	private Position position;
+	private Direction direction;
+	
+	
+	public Robot (Position position, Direction direction) {
+		this.direction=direction;
+		this.position=position; 
+		
+	}
 
+	public void avencer () {
+		
+	}
+	public void tourner() {
+		
+	}
 }
--- exo3_3/src/main/java/fr/uvsq/exo3_3/RobotStatique.java
+++ exo3_3/src/main/java/fr/uvsq/exo3_3/RobotStatique.java
@@ -1,5 +1,15 @@
 package fr.uvsq.exo3_3;
 
 public final class RobotStatique {
-
+	private Direction direction;
+	
+	public RobotStatique(Direction direction ) {
+		
+		this.direction=direction;
+		
+	}
+	
+	public void tourner() {
+		
+	}
 }
